f39fd461cfdc636015f4de4094f4a76213b4f016
Jay Betancourt <oclean66@gmail.com>
2018-06-22 10:55:36 -0500

    asia update

--- src/components/resultslive.js
+++ src/components/resultslive.js
@@ -42,53 +42,52 @@ const Resultslive = ({ name }) => (
             <div className="tab-content">
                 <div id="Baseball" className="tab-pane fade in active show">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore05" id="inscore-xdc-540333" src="https://www.livescore.in/free/540333/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore00" id="inscore-xdc-182649" src="http://www.livescore.in/free/182649/" width="900" height="1000" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Soccer" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore01" id="inscore-xdc-844206" src="https://www.livescore.in/free/844206/" width="1000" height="1200" frameBorder="0" scrolling="no"></iframe>
+                        <iframe title="livescore01" id="inscore-xdc-336722" src="http://www.livescore.in/free/336722/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Tennis" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore02" id="inscore-xdc-226068" src="http://www.livescore.in/free/226068/" width="624" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore02" id="inscore-xdc-505531" src="http://www.livescore.in/free/505531/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Basketball" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore03" id="inscore-xdc-974024" src="http://www.livescore.in/free/974024/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
-
+                        <iframe title="livescore03" id="inscore-xdc-694009" src="http://www.livescore.in/free/694009/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Hockey" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore04" id="inscore-xdc-903961" src="https://www.livescore.in/free/903961/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>                    </div>
+                        <iframe title="livescore04" id="inscore-xdc-765906" src="http://www.livescore.in/free/765906/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
+                    </div>
                 </div>
 
                 <div id="AM-Football" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore06" id="inscore-xdc-101042" src="https://www.livescore.in/free/101042/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore06" id="inscore-xdc-754328" src="http://www.livescore.in/free/754328/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Handball" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore07" id="inscore-xdc-827582" src="https://www.livescore.in/free/827582/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore07" id="inscore-xdc-420521" src="http://www.livescore.in/free/420521/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Volleyball" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore08" id="inscore-xdc-480657" src="https://www.livescore.in/free/480657/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
-                    </div>
+                        <iframe title="livescore08" id="inscore-xdc-618361" src="http://www.livescore.in/free/618361/" width="624" height="500" frameborder="0" scrolling="no"></iframe>                    </div>
                 </div>
                 <div id="RugbyU" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore09" id="inscore-xdc-485604" src="https://www.livescore.in/free/485604/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore09" id="inscore-xdc-847679" src="http://www.livescore.in/free/847679/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
                 <div id="Cricket" className="tab-pane fade">
                     <div className="table-responsive-lg">
-                        <iframe title="livescore19" id="inscore-xdc-734675" src="https://www.livescore.in/free/734675/" width="1000" height="1200" frameborder="0" scrolling="no"></iframe>
+                        <iframe title="livescore10" id="inscore-xdc-878005" src="http://www.livescore.in/free/878005/" width="624" height="500" frameborder="0" scrolling="no"></iframe>
                     </div>
                 </div>
 
